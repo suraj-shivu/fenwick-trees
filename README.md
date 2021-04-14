078dde23809413be160a226374deeb7015493df0
suraj-shivu <surajshivu2000@gmail.com>
2021-04-14 16:52:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# fenwick-trees
---
+++ fenwick.h
@@ -0,0 +1,14 @@
+#include<iostream> 
+#include<algorithm>
+#include<iterator>
+
+template <typename T>
+class FenwickTree{
+    private:
+        T[] tree;
+        int length;
+
+    public:
+        ConstructTree();
+
+};
