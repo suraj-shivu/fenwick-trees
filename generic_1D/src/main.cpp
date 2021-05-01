c46e8aae6805ce78ce352ce9d2cf6ce6a0bbbaf7
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2021-05-01 19:23:56 +0530

    Converting commit 0c15721b2303c7f4dcddb80ad53449feaedafa97 to cmake format

--- generic_1D/src/main.cpp
+++ generic_1D/src/main.cpp
@@ -9,5 +9,21 @@ int main(){
     std::cout<<a.pointQuery(5)<<std::endl;
     std::cout<<a.rangeQuery(4,9)<<std::endl;
     a.display();
+    
+    //stl testing
+
+    //find
+    FenwickTree<int>::Iterator p=std::find(a.begin(), a.end(), 3);
+    if(p!=a.end())
+        std::cout<<"Element found: "<<*p<<" at position "<<p-a.begin()<<std::endl;
+    else
+        std::cout<<"Element not found"<<std::endl;     
+    //print
+    for(auto i: a)
+    std::cout<<i<<std::endl;
+    //fill
+    std::fill(a.begin(), a.end(), 100);
+    for(auto i: a)
+    std::cout<<i<<std::endl;
     return 0;
 }
--- include/generic_1D/fenwick.hpp
+++ include/generic_1D/fenwick.hpp
@@ -8,11 +8,37 @@ class FenwickTree{
         T* BIT;
         int size;
     public:
+
+	struct Iterator 
+	{
+		using iterator_category = std::bidirectional_iterator_tag;
+		using difference_type   = std::ptrdiff_t;
+		using value_type        = T;
+		using pointer           = T*;
+		using reference         = T&;
+
+		Iterator(pointer ptr) : m_ptr(ptr) {}
+
+		reference operator*() const { return *m_ptr; }
+		pointer operator->() { return m_ptr; }
+		Iterator& operator++() { m_ptr++; return *this; }  
+		Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
+		friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
+		friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; };
+		friend T operator- (const Iterator a, const Iterator b) { return a.m_ptr - b.m_ptr; };
+		friend T operator+ (const Iterator a, const Iterator b) { return a.m_ptr + b.m_ptr; };    
+
+		private:
+		pointer m_ptr;
+	};
+
+    Iterator begin() { return Iterator(&BIT[0]); }
+    Iterator end()   { return Iterator(&BIT[size+1]); }
 	FenwickTree(int n){
 		size = n;
 		BIT = new T[n+1];
 		for(int i=0;i<=size;i++){
-			BIT[i]=0;
+			BIT[i]=T();
 		}
 	};
 	void constructTree();
